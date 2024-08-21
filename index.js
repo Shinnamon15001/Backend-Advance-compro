1d55f58ffd36fc6ccf4adb613a121039bae59bd2
Shinnamon15001 <s6606022610030@email.kmutnb.ac.th>
2024-08-21 08:27:22 +0700

    i forgot to commit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 
-const express = require("express")
-const app = express()
-const port = process.env.PORT;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use 3000 as a fallback if PORT is not defined
 
-app.get("/", (req,res) => {
-    res.send("Hello world!"); });
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
 
-app.listen(port,
-    () => {
-        console.log(`Example app at http://localhost:${port}`);
-    }
-);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
